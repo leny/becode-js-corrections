514e9e8fff33313d76cda84b35b3c502d691c810
ValdenBE <pirghaye.f@gmail.com>
2019-06-24 16:28:32 +0200

    Correction 2.6

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,20 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(()=> {
-
-    // your code here
-
+(() => {
+    let x = 1;
+    while (x <= 100) {
+        let msg = "";
+        if (x % 3 === 0) {
+            msg += "Fizz";
+        }
+        if (x % 5 === 0) {
+            msg += "Buzz";
+        }
+        if (msg === "") {
+            msg = x;
+        }
+        console.log(msg);
+        x++;
+    }
 })();
