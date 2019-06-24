698221a5bd911bbb189d70b7263e6288a720972d
Ashraf <ashraf@post.com>
2019-06-24 16:29:17 +0200

    Corrections Ashraf

--- 05-arrays/02-manipulate-array/script.js
+++ 05-arrays/02-manipulate-array/script.js
@@ -23,5 +23,43 @@
         "cerise",
     ];
 
-    // your code here
+    // ON CLICK
+    document.getElementById("run").addEventListener("click", function () {
+
+        // POSSIBLE SOLUTION (SHORTEST ONE, FASTER)
+
+        fruits[0] = "banane";
+        fruits[fruits.length - 1] = "kiwi";
+        console.log(fruits);
+
+        // POSSIBLE SOLUTION (FINDS AND CHANGES THE SPECIFIED INDEX, SLOWER)
+
+        /* fruits.splice(0, 1); // or .shitf();
+        fruits.splice(-1, 1); // or .pop();
+        fruits.unshift("banane");
+        fruits.push("kiwi"); */
+
+
+        // POSSIBLE SOLUTION (FIND ALL MATCHES, SLOWEST)
+        /* for (let i = 0; i <= fruits.length; i++) {
+            if (fruits[i] === "pomme") {
+                fruits[i] = "banane";
+            }
+            if (fruits[i] === "cerise") {
+                fruits[i] = "kiwi";
+            }
+        } */
+
+        // POSSIBLE SOLUTION (FIND ALL MATCHES, SLOWEST)
+
+        /* fruits.forEach(fruit => {
+            if (fruit === "pomme") {
+                fruit = "banane";
+            }
+            if (fruit === "cerise") {
+                fruit = "kiwi";
+            }
+        }); */
+
+    });
 })();
