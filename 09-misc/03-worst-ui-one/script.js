89de48ee5c73571cb3be23c6a719a7ba57fa2aee
leny <info@flatland.be>
2019-06-25 15:23:19 +0200

    Add 09.03

--- 09-misc/03-worst-ui-one/script.js
+++ 09-misc/03-worst-ui-one/script.js
@@ -10,5 +10,9 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    const target = document.getElementById("target");
+
+    document.getElementById("slider").addEventListener("input", evt => {
+        target.innerText = `+${evt.currentTarget.value}`;
+    });
 })();
