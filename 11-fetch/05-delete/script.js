c1503f48c130660962142ae6c287fd2be729adda
Ashraf <ashraf@post.com>
2019-07-04 15:36:16 +0200

    Ashraf's corrections, 11.4 and 11.5

--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -10,5 +10,52 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    const apiURL = "http://localhost:3000/heroes";
+    let heroID = document.getElementById("hero-id");
+
+    document.getElementById("run").addEventListener("click", function () {
+
+        let heroIDValue = heroID.value.trim();
+
+        (async () => {
+
+            if (heroIDValue !== "" | " ") {
+
+
+                const response = await fetch(apiURL);
+                const responseContent = await response.json();
+
+                responseContent.forEach(async (hero, index) => {
+                    let idCheck = "not-found";
+                    if (heroIDValue == hero.id) {
+                        idCheck = "found";
+                        const responseFetch = await fetch(apiURL + "/" + heroIDValue, {
+                            method: "DELETE"
+                        }).then(function (value) {
+                            alert("The hero number " + heroIDValue + " has been deleted successfully");
+                        });
+
+                        const logResponse = await fetch(apiURL);
+                        const logResponseContent = await logResponse.json();
+
+                        console.log(logResponseContent);
+                    } else {
+                        if (index === responseContent.length - 1 && idCheck === "not-found") {
+                            alert("There're " + responseContent.length + " heroes to DELETE one, simply enter an ID, \nexample: 3");
+                        }
+                    }
+                });
+                if (responseContent.length <= 0) {
+                    if (window.confirm("There's no more heroes, click OK to add one 😉 \nDon't forget to reload this page when you come back 😛")) {
+                        window.open("http://localhost:8000/11-fetch/04-add/index.html");
+                    };
+                }
+            } else {
+                alert("An hero ID is required");
+            }
+
+        })();
+    });
+
 })();
